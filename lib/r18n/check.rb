ae4ee7887f76ce1769841437725dc4c21e546a4a
teamiguana <teamiguana@shoppydoo.com>
2012-06-18 14:17:57 +0200

    check for bad translations

--- lib/r18n/check.rb
+++ lib/r18n/check.rb
@@ -19,6 +19,28 @@ module R18n
       end
     end
 
+    def check_bad_word(translation, bad_word)
+      data = translation.instance_eval { @data }
+      if data.nil?
+        return ''
+      end
+      data.each do |key, val|
+        if val.class == R18n::TranslatedString
+          assert_is_key_correct(translation, key, val, bad_word)
+        elsif val.class == R18n::Typed
+          val.value.each{|key,value| assert_is_key_correct(translation, data.first[0], value, bad_word)}
+        else
+          check_bad_word(translation.send(key), bad_word)
+        end
+      end
+    end
+
+    def assert_is_key_correct(translation, key, value, bad_word)
+      if value.to_s.match(/#{bad_word}/i)
+        raise "problems on translation <<#{key}>> of <<#{translation.instance_eval { @locale }.code}>>"
+      end
+    end
+
     def assert_typed_pl_keys(data, key, other)
       evaluated = other.instance_eval { @data }
       evaluated.each do |k, v|
--- test/r18n_check_test.rb
+++ test/r18n_check_test.rb
@@ -7,15 +7,55 @@ require '../lib/r18n/check'
 
 class R18nCheckTest < Test::Unit::TestCase
   def test_it_signals_missing_stuff
-
-    italian=R18n::I18n.new('it-it', File.join(File.dirname(__FILE__),'..','i18n'))
-    spanish=R18n::I18n.new('es-es', File.join(File.dirname(__FILE__),'..','i18n'))
-    french=R18n::I18n.new('fr-fr', File.join(File.dirname(__FILE__),'..','i18n'))
+    italian=translations_for('it-it')
+    spanish=translations_for('es-es')
+    french=translations_for('fr-fr')
 
     translations = italian
 
     [spanish, french].each do |translation|
       R18n::Check.new.check(translations.t, translation.t)
     end
+  end
+
+  def test_simple_key_not_contains_bad_word
+    assert_no_problem_for(translations_for('simplekey_ok'))
+  end
+
+  def test_simple_key_contains_bad_word
+    trans=translations_for('simplekey_ko')
+    assert_raise_problem_for(trans, 'simplekey_ko', 'key')
+  end
+
+  def test_subkey_not_contains_bad_word
+    assert_no_problem_for(translations_for('subkey_ok'))
+  end
+
+  def test_subkey_contains_bad_word
+    trans=translations_for('subkey_ko')
+    assert_raise_problem_for(trans, 'subkey_ko', 'subkey')
+  end
+
+  def test_specialkey_not_contains_bad_word
+    assert_no_problem_for(translations_for('specialkey_ok'))
+  end
+
+  def test_specialkey_contains_bad_word
+    trans=translations_for('specialkey_ko')
+    assert_raise_problem_for(trans, 'specialkey_ko', 'key')
+  end
+
+  def translations_for(filename)
+    R18n::I18n.new(filename, File.join(File.dirname(__FILE__), '..', 'i18n'))
+  end
+
+  def assert_no_problem_for(trans)
+    assert_nothing_raised { R18n::Check.new.check_bad_word(trans.t, 'bad') }
+  end
+
+  def assert_raise_problem_for(trans, filename, key)
+    assert_raise_message("problems on translation <<#{key}>> of <<#{filename}>>") {
+      R18n::Check.new.check_bad_word(trans.t, 'bad')
+    }
   end
 end
