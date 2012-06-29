a0a35dbee98f38342615bcbf8888ed839fde97c9
teamiguana <teamiguana@shoppydoo.com>
2012-06-29 14:48:48 +0200

    checks using yaml instead of R18n

---
+++ lib/r18n/yaml_check.rb
@@ -0,0 +1,55 @@
+require "yaml"
+
+module R18n
+  class YamlCheck
+    def check(file_name, block)
+      file="#{file_name}.yml"
+      root = YAML::load(IO.read("#{file}"))
+      root.each { |key, value| traverse({key => value}, file, [], block) }
+    end
+
+    def traverse(node, file, path, block)
+      path = path.dup << node.keys.first
+      value = node.values.first
+      if !value.is_a?(Hash)
+        block.call(value, file, path)
+      else
+        node.values.first.each { |k, v| traverse({k => v}, file, path, block) }
+      end
+    end
+
+    def check_presence(main_t, to_check_t)
+      to_check_file_name="#{to_check_t}.yml"
+      other_t = YAML::load(IO.read("#{to_check_file_name}"))
+      block = lambda { |value, file, path|
+        assert_presence(path, other_t, to_check_file_name)
+      }
+      check(main_t, block)
+    end
+
+    def assert_presence(path, to_check_t, to_check_file_name)
+      current_path = to_check_t
+      path.each do |xx|
+        current_path = current_path[xx]
+      end
+      raise_with_message(to_check_file_name, path) if current_path.nil?
+    end
+
+    def check_bad_word(file_name, bad_word)
+      block = lambda { |value, file, path|
+        assert_is_key_correct(value, file, bad_word, path)
+      }
+      check(file_name, block)
+    end
+
+    def assert_is_key_correct(value, file, bad_word, path)
+      if value.to_s.match(/#{bad_word}/i)
+        raise_with_message(file, path)
+      end
+    end
+
+    def raise_with_message(file, path)
+      raise "problems on translation [#{path.join(' -> ')}] of <<#{file}>>"
+    end
+  end
+end
--- test/r18n_check_test.rb
+++ test/r18n_check_test.rb
@@ -3,59 +3,65 @@ require 'bundler/setup'
 require 'test/unit'
 require 'r18n-core'
 
-require '../lib/r18n/check'
+require '../lib/r18n/yaml_check'
+
 
 class R18nCheckTest < Test::Unit::TestCase
+  def test_it_signals_nothing_beause_correct
+    italian=file_full_path('it-it')
+    french=file_full_path('fr-fr')
+
+    R18n::YamlCheck.new.check_presence(italian, french)
+  end
+
   def test_it_signals_missing_stuff
-    italian=translations_for('it-it')
-    spanish=translations_for('es-es')
-    french=translations_for('fr-fr')
+    italian=file_full_path('it-it')
+    spain=file_full_path('es-es')
 
-    translations = italian
-
-    [spanish, french].each do |translation|
-      R18n::Check.new.check(translations.t, translation.t)
-    end
+    assert_raise_message("problems on translation [b -> leaf2] of <<#{file_full_path('es-es')}.yml>>") {
+      R18n::YamlCheck.new.check_presence(italian, spain)
+    }
   end
 
   def test_simple_key_not_contains_bad_word
-    assert_no_problem_for(translations_for('simplekey_ok'))
+    assert_no_problem_for(file_full_path('simplekey_ok'))
   end
 
   def test_simple_key_contains_bad_word
-    trans=translations_for('simplekey_ko')
-    assert_raise_problem_for(trans, 'simplekey_ko', 'key')
+    assert_raise_problem_for(file_full_path('simplekey_ko'), 'key')
   end
 
   def test_subkey_not_contains_bad_word
-    assert_no_problem_for(translations_for('subkey_ok'))
+    assert_no_problem_for(file_full_path('subkey_ok'))
   end
 
   def test_subkey_contains_bad_word
-    trans=translations_for('subkey_ko')
-    assert_raise_problem_for(trans, 'subkey_ko', 'subkey')
+    assert_raise_problem_for(file_full_path('subkey_ko'), 'key -> subkey2 -> subkey3')
   end
 
   def test_specialkey_not_contains_bad_word
-    assert_no_problem_for(translations_for('specialkey_ok'))
+    assert_no_problem_for(file_full_path('specialkey_ok'))
+  end
+
+  def test_more_key_contains_bad_word
+    assert_raise_problem_for(file_full_path('more_key_ko'), 'b -> leaf2')
   end
 
   def test_specialkey_contains_bad_word
-    trans=translations_for('specialkey_ko')
-    assert_raise_problem_for(trans, 'specialkey_ko', 'key')
+    assert_raise_problem_for(file_full_path('specialkey_ko'), 'key -> n')
   end
 
-  def translations_for(filename)
-    R18n::I18n.new(filename, File.join(File.dirname(__FILE__), '..', 'i18n'))
+  def file_full_path(filename)
+    File.join(File.dirname(__FILE__), '..', 'i18n', filename)
   end
 
-  def assert_no_problem_for(trans)
-    assert_nothing_raised { R18n::Check.new.check_bad_word(trans.t, 'bad') }
+  def assert_no_problem_for(file_name)
+    assert_nothing_raised { R18n::YamlCheck.new.check_bad_word(file_name, 'bad') }
   end
 
-  def assert_raise_problem_for(trans, filename, key)
-    assert_raise_message("problems on translation <<#{key}>> of <<#{filename}>>") {
-      R18n::Check.new.check_bad_word(trans.t, 'bad')
+  def assert_raise_problem_for(filename, key)
+    assert_raise_message("problems on translation [#{key}] of <<#{filename}.yml>>") {
+      R18n::YamlCheck.new.check_bad_word(filename, 'bad')
     }
   end
 end
